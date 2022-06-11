c0b09807091d40f72e9c831d9ec6c90502596989
RV <38699422+venchev@users.noreply.github.com>
2022-06-11 18:06:46 +0300

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo
+echo WARNING:  This script will remove your VM with KubeFlow and everything related. 
+echo Are you sure, that you want this?
+echo 
+echo if Yes - the script will await 10 seconds, and you can press Ctrl-C to stop it.
+echo
+echo if you want to continue, just await 15 secs and it will start automatically. 
+echo No undo is possible !!!
+echo
+echo Counting 15 seconds NOW.
+sleep 10
+echo
+echo 10 seconds have passed. Starting removal of the VM resources and cleanup!
+echo
+vagrant destroy -f ; rm -rf .vagrant ; rm -rf .git ; vagrant global-status
+echo
+echo Done Successfully!
